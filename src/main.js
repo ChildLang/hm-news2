0db399e110cba8110014c43dcf33da73fa55cbd5
cestmoi <2421243448@qq.com>
2020-07-02 15:52:26 +0800

    123

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-
-Vue.config.productionTip = false
+import 'amfe-flexible'
 
 new Vue({
-  router,
-  render: h => h(App)
+  render: c => c(App)
 }).$mount('#app')
